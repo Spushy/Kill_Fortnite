b67a9fee20157cca96f71c146c3829468a52a289
Spushy <101425306+Spushy@users.noreply.github.com>
2022-07-06 22:56:38 +0200

    Add files via upload

---
+++ kill_fortnite.bat
@@ -0,0 +1,5 @@
+taskkill/im FortniteLauncher.exe /F
+taskkill/im FortniteClient-Win64-Shipping_EAC.exe /F
+taskkill/im FortniteClient-Win64-Shipping_BE.exe /F
+taskkill/im FortniteClient-Win64-Shipping.exe /F
+pause
